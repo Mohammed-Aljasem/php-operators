2b1f8230c25289a72edb07fd3eb415f8b92ed580
mohammed-aljasem <moh.m.a;jasem@gmail.com>
2021-01-20 09:02:13 +0200

    19, jan

---
+++ operetor.php
@@ -0,0 +1,168 @@
+<?php
+//=============Q1==============
+// $x = 8;
+// if ($x % 4 == 0) {
+//   echo "$x This year is leap";
+// } elseif ($x % 4 != 0) {
+//   echo "$x This year is leap";
+// } else {
+//   echo "please enter number year";
+// }
+
+
+//=============Q2==============
+
+// $x = 61;
+// if ($x <= 0) {
+//   echo "you are in winter and you need to wear so many clothes because the Weather is so cold ";
+// } elseif ($x >= 0 && $x <= 20) {
+//   echo "you are in winter and you need to wear a coat because the Weather is cold a little";
+// } elseif ($x >= 21 && $x <= 60) {
+//   echo "you are in summer and you don't need to wear so many clothes because the Weather is hot  ";
+// } elseif ($x >= 61 && $x <= 100) {
+//   echo "You must move well for your body to starch evenly";
+// } else {
+//   echo "Error";
+// }
+
+
+//=============Q3==============
+
+// $x = 4;
+// $z = 4;
+// if ($z != $x) {
+//   echo $x + $z;
+// } elseif ($z == $x) {
+//   echo ($z + $x) * 3;
+// }
+
+//=============Q4==============
+// $x = 150;
+// $z = 100;
+// $a = $x - $z;
+
+// abs($a);
+// if (100 <= $a) {
+//   echo abs(3 * $a);
+// } elseif (100 > $a) {
+//   echo abs($a);
+// }
+
+
+//=============Q5==============
+// $x = 16;
+// $z = 30;
+// $a = $x + $z;
+
+
+// if ($a == 30) {
+//   echo "true";
+// } elseif ($z == 30 || $x == 30) {
+//   echo "true";
+// } else {
+//   echo $a;
+// }
+
+//=============Q6==============
+// $a = 222;
+
+// if (90 >= $a) {
+//   echo "false";
+// } elseif (91 <= $a && 110 >= $a) {
+//   echo "true";
+// } elseif (111 <= $a && 190 >= $a) {
+//   echo "in between";
+// } elseif (191 <= $a && 210 >= $a) {
+//   echo "true";
+// } elseif (211 <= $a) {
+//   echo "false";
+// }
+
+//=============Q7==============
+// $a = 21;
+// if ($a % 3 == 0 && $a % 7 == 0) {
+//   echo "$a multiple 3 and 7";
+// } elseif ($a % 3 == 0) {
+//   echo "$a multiple 3";
+// } elseif ($a % 7 == 0) {
+//   echo "$a multiple  7";
+// }
+
+//=============Q8==============
+
+
+// if (ltrim($a, "if")) {
+
+//   echo $a;
+// }
+
+
+
+//=============Q10==============
+// $z = 155;
+// $x = 140;
+
+// if (($z > 100 &&  200 > $z) && ($x > 100 &&  200 > $x)) {
+//   echo "$z and $x in the range";
+// } else {
+//   echo "the numbers isn't the range";
+// }
+// //=============Q11==============
+// $z = 51;
+// $x = 140;
+
+// if (($z >= 20 &&  50 >= $z) || ($x >= 20 &&  50 >= $x)) {
+//   echo "true";
+// } else {
+//   echo "false";
+// }
+
+
+//=============Q12==============
+// $w = 15;
+// $q = 23;
+// $e = 11;
+
+// if ($e > $q && $e > $w) {
+//   echo $e;
+// } elseif ($w > $q && $w > $e) {
+//   echo $w;
+// } elseif ($q > $w && $q > $e) {
+//   echo $q;
+// }
+//=============Q13==============
+// function hello($x, $z)
+// {
+//   $a = $x + 100;
+//   $s = $z + 100;
+
+
+//   if ($x < 100 && $s < $a) {
+//     abs($x);
+//     echo $x . " is close";
+//   } elseif ($z < 100 && $s > $a) {
+//     abs($z);
+//     echo $z . " is close";
+//   } elseif ($x > 100 && $s > $a) {
+//     abs($x);
+//     echo $x . " is close";
+//   } elseif ($z > 100 && $s < $a) {
+//     abs($z);
+//     echo $z . " is is close";
+//   }
+// };
+
+// hello(1, 2);
+//=============Q14==============
+// $x = 6;
+// $z = 30;
+// $a = $x - $z;
+
+// abs($a);
+// if ($a == 5) {
+//   echo $x + $z . "is sum";
+// } elseif ($z == 5 || $x == 5) {
+//   echo $x + $z . "is sum";
+// } else {
+//   echo "test";;
+// }
